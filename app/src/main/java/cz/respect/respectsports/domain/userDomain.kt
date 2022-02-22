4894c38cad600ee92a48036c3b7963550d189c2c
tomasKopecky <kopeck32@student.vspj.cz>
2022-02-22 18:10:59 +0100

    New match basics done Players import to the new match spinner done

--- app/src/main/java/cz/respect/respectsports/domain/userDomain.kt
+++ app/src/main/java/cz/respect/respectsports/domain/userDomain.kt
@@ -3,8 +3,4 @@ package cz.respect.respectsports.domain
 data class User(val id: String,
                       val name: String,
                       val token: String?) {
-
-    fun encryptToken() {
-        val cipherText: ByteArray = crypto.encrypt(plainText) // https://github.com/facebookarchive/conceal
-    }
 }
